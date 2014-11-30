98ad9dad89621c8bdffde304afc2af9cdca704fe
sarah1985 <sarahmleavy@gmail.com>
2014-11-30 14:37:31 -0500

    initial

---
+++ task_02.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""Task 02: Lesson 14"""
+
+import numpy
+from pprint import pprint
+
+def numpy_task02(param_mean, param_sd):
+    """list of normally distributed random numbers"""
+
+    normal_list = []
+    for num in range(1, 100):
+        normal_list.append(numpy.random.normal(param_mean, param_sd))
+
+    return normal_list
+
+if __name__ == "__main__":
+    pprint(numpy_task02(100, 10))
