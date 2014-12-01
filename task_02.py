05fa71ecc478f0b6350c4d8117fbbbc1d2a52d8d
sarah1985 <sarahmleavy@gmail.com>
2014-11-30 19:41:21 -0500

    task 2

--- task_02.py
+++ task_02.py
@@ -10,7 +10,7 @@ def numpy_task02(param_mean, param_sd, rep):
     """list of normally distributed random numbers
     this will be helpful for modeling and statistical analysis """
 
-    return [numpy.random.normal(param_mean, param_sd, rep)]
+    return numpy.random.normal(param_mean, param_sd, rep)
 
 if __name__ == "__main__":
     pprint(numpy_task02(100, 10, 100))
